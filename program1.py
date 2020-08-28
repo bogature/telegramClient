ff1891f4a3e4cb529b9122971d99ea6879be0b34
Andrey <bogature@i.ua>
2020-08-28 18:54:41 +0300

    first commit

---
+++ program1.py
@@ -0,0 +1,31 @@
+print("hello")
+
+from telethon import TelegramClient, events
+
+api_id = 1584723
+api_hash = '332a33d701c3740a65ec9d77377fb434'
+client = TelegramClient('+380994430922', api_id, api_hash)
+
+listen_for_ids = (
+    1073702594,
+)
+
+forward_to_ids = (
+    -1001243390227,
+)
+
+
+@client.on(events.NewMessage)
+async def my_event_handler(message):
+    if message.sender_id in listen_for_ids:
+        for receiver_id in forward_to_ids:
+            sender = await message.get_sender()
+            await client.send_message(
+                receiver_id,
+                f'Отримано сигнал. Канал: {sender.username} (id: {sender.id})!\nВМІСТ: (лише текст)\n{message.raw_text}'
+            )
+
+client.start()
+print(f'Started. Listening for IDs: {listen_for_ids}')
+print(f'Forwarding to IDs: {forward_to_ids}')
+client.run_until_disconnected()
